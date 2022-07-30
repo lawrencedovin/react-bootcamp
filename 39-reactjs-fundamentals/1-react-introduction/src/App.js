63df64f9cf456828a6c80122c25897d45b1f8e0c
Lawrence Dovin <lawrencedovin@gmail.com>
2022-07-29 20:41:45 -0400

    Added movie list example loop

---
+++ 39-reactjs-fundamentals/1-react-introduction/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import MovieList from './components/MovieList/MovieList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MovieList />
+    </div>
+  );
+}
+
+export default App;

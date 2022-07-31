5001c739c09f93f5b45906263e34a7f7e8e272a6
Lawrence Dovin <lawrencedovin@gmail.com>
2022-07-30 21:23:48 -0400

    Added reusable dog component

--- 39-reactjs-fundamentals/1-react-introduction/src/App.js
+++ 39-reactjs-fundamentals/1-react-introduction/src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import MovieList from './components/MovieList/MovieList';
-import Shiba from './components/Shiba/Shiba';
+import Dog from './components/Dog/Dog';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <MovieList />
-      <Shiba src="https://images.unsplash.com/photo-1550035605-144be67b33ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80" />
+      <Dog src="https://images.unsplash.com/photo-1550035605-144be67b33ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80" />
+      <Dog src="https://images.unsplash.com/photo-1619979842913-08603df442de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
     </div>
   );
 }
--- 39-reactjs-fundamentals/1-react-introduction/src/components/Shiba/Shiba.js
+++
@@ -1,5 +0,0 @@
-const Shiba = (props) => {
-    return <img src={props.src} />
-}
-
-export default Shiba;

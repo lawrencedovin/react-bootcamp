4f371fa078243f7204f617dbc0d4b4868f67aaa2
Lawrence Dovin <lawrencedovin@gmail.com>
2022-08-13 17:57:41 -0400

    Added space logo

--- 39-reactjs-fundamentals/3-create-react-app/src/App.js
+++ 39-reactjs-fundamentals/3-create-react-app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import ShoppingList from './ShoppingList/ShoppingList';
 import items from './items';
+import logo from './images/space-logo.svg'
 
 function App() {
   return (
     <>
+      <img src={logo} alt={logo} height="50px" />
       <ShoppingList items={items} username="lawrence" />
     </>
   );

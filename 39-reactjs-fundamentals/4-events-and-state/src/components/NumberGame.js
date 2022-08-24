9434b4c1b58cf7734eb72f9dec0c405cb60b5792
Lawrence Dovin <lawrencedovin@gmail.com>
2022-08-24 19:05:19 -0400

    Added solution

--- 39-reactjs-fundamentals/4-events-and-state/src/components/NumberGame.js
+++ 39-reactjs-fundamentals/4-events-and-state/src/components/NumberGame.js
@@ -2,28 +2,31 @@ import React, {useState} from "react";
 import generateRandomNumber from "../utils/generateRandomNumber";
 
 const NumberGame = () => {
-    const [guess, setGuess] = useState(null);
+    const [guess, setGuess] = useState(0);
     const [randomNumber, setRandomNumber] = useState(generateRandomNumber);
+    const [hasWon, setHasWon] = useState(false);
 
     const newRandomNumber = () => {
         setRandomNumber(generateRandomNumber);
     }
 
     const handleChange = (event) => {
-        setGuess(event.target.value);
+        setGuess(parseInt(event.target.value));
     }
 
     const checkGuess = () => {
-
+        guess === randomNumber ? setHasWon(true) : setHasWon(false);
+        console.log(`Guess: ${guess} Random Number: ${randomNumber}`);
     }
 
     return (
         <>
             <h1>Your Guess: {guess}</h1>
-            <h1>Random Number: {randomNumber}</h1>
+            {/* <h1>Random Number: {randomNumber}</h1> */}
+            <h1>Answered Correctly? {hasWon ? "Congrats you Won!" : "Incorrect Guess"} </h1>
             <input type="number" min="1" max="10" value={guess} onChange={handleChange}/>
-            <button onClick={() => checkGuess}>Confirm Guess</button>
-            <button onClick={newRandomNumber}>Generate Number</button>
+            <button onClick={checkGuess}>Confirm Guess</button>
+            <button onClick={newRandomNumber}>Generate New Random Number</button>
         </>
     )
 }
